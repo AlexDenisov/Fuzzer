ef2325be4626f73986f6235a2558db2c944ed0b3
AlexDenisov <1101.debian@gmail.com>
2016-03-07 21:16:56 +0100

    Add Mutation module

---
+++ Mutation/Mutation.h
@@ -0,0 +1,19 @@
+//
+//  Mutation.h
+//  Mutation
+//
+//  Created by AlexDenisov on 07/03/16.
+//  Copyright © 2016 lowlevelbits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Mutation.
+FOUNDATION_EXPORT double MutationVersionNumber;
+
+//! Project version string for Mutation.
+FOUNDATION_EXPORT const unsigned char MutationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mutation/PublicHeader.h>
+
+
